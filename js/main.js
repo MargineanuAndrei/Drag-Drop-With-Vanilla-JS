45ae39050e957e61e20b4eaf2cba204e464e7308
MargineanuAndrei <margineanuandrei94@gmail.com>
2018-05-30 10:58:25 +0300

    add first functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log('Runing');
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+function dragStart() {
+	this.className += ' hold';
+	setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+	this.className = 'fill';
+}
+
+// Image block events listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
